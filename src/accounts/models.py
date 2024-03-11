725a4ec3ae723f439684029bf6222a2906777022
Muhib Al Hasan <muhibalhasan1@gmail.com>
2024-03-11 13:02:22 +0600

    Wrote Address model

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -1,6 +1,20 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django_countries.fields import CountryField
 
 
 class User(AbstractUser):
     phone = models.CharField(max_length=20)
+
+
+class Address(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='addresses'
+    )
+
+    house = models.CharField()
+    street = models.CharField()
+    city = models.CharField()
+    state = models.CharField()
+    country = CountryField()
+    post_code = models.CharField()
