32d4a87460e70ec59c72598f3e1b6c1eeb140f96
Ahmed Symum Swapno <ahmed.symum.swapno@g.bracu.ac.bd>
2024-03-15 11:07:40 +0600

    Added min value for book quantity

--- src/books/models.py
+++ src/books/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
 from src.core.models import BaseModel
@@ -34,7 +35,7 @@ class Book(BaseModel):
     title = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    quantity = models.IntegerField()
+    quantity = models.IntegerField(validators=[MinValueValidator(0)])
     language = models.CharField(max_length=3, choices=LanguageChoices)
     genre = models.CharField(max_length=20, choices=GenreChoices.choices)
     author = models.ForeignKey(
