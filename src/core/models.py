71a2845820865672a3e32dcfb1cb9a828a2ae642
Muhib Al Hasan <muhibalhasan1@gmail.com>
2024-03-18 16:07:39 +0600

    Soft delete added in BaseModel

--- src/core/models.py
+++ src/core/models.py
@@ -1,9 +1,27 @@
 from django.db import models
 
 
+class SoftDeletionManager(models.Manager):
+
+    def get_queryset(self):
+        return super().get_queryset().filter(deleted=False)
+
+
 class BaseModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    deleted = models.BooleanField(default=False)
+
+    objects = SoftDeletionManager()
+    all_objects = models.Manager(
+    )  # Manager to access all objects, including soft-deleted ones
+
+    def delete(self, using=None, keep_parents=False):
+        self.deleted = True
+        self.save()
+
+    def hard_delete(self):
+        super().delete()
 
     class Meta:
         abstract = True
