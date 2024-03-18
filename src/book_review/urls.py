07d621b183d2269fd30159b3dd5ed4470201cacf
dattar321 <dattar321@gmail.com>
2024-03-18 16:16:09 +0600

    update-review

--- src/book_review/urls.py
+++ src/book_review/urls.py
@@ -1,24 +1,20 @@
 from django.urls import path
 
-from .views import ReviewCreateView
+from .views import BookReviewListView, ReviewCreateView, ReviewUpdateView
 
 app_name = 'review'
 
 reviewurlpatterns = [
-    # path(
-    #     'book/<int:pk>/',
-    #     BookReviewListView.as_view(),
-    #     name='review_list_book'
-    # ),
-    # path(
-    #     'user/<int:pk>/',
-    #     UserReviewListView.as_view(),
-    #     name='review_list_user'
-    # ),
+    path('<int:pk>/reviews/', BookReviewListView.as_view(), name='reviews'),
+    path(
+        '<int:pk>/update_review/',
+        ReviewUpdateView.as_view(),
+        name='update-review'
+    ),
     path(
         '<int:pk>/create_review/',
         ReviewCreateView.as_view(),
-        name='create_review'
+        name='create-review'
     ),
     # path('update_review/',ReviewUpdateView.as_view(), name="update_view")
 ]
