48ba1bb7d1071ddb0583480de143d8329b46fed9
Muhib Al Hasan <muhibalhasan1@gmail.com>
2024-03-19 12:13:21 +0600

    added redis conf and wrote cached view mixin

--- src/project/settings/app.py
+++ src/project/settings/app.py
@@ -9,3 +9,14 @@ MEDIA_ROOT = BASE_DIR / 'media'  # type: ignore # noqa
 STATIC_ROOT = 'staticfiles'
 
 LOGIN_REDIRECT_URL = '/'
+
+# cache
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379/1',
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        }
+    }
+}
